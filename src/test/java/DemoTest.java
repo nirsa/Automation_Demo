b2bb39a73eba1d3af14520535df4a6caef73c40f
nirsa <nir.sarusy@gmail.com>
2017-12-11 17:50:01 +0200

    Git upload

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,109 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class DemoTest {
+    @Test
+    public void test1() {
+        String expected = "C:\\Jenkins_Deploy\\Automation_Demo\\Good.txt\\";
+        String results = "C:\\Jenkins_Deploy\\Automation_Demo\\TestResults.txt\\";
+        String cmdrun = "cmd /c start cmd.exe /K \"C:\\Jenkins_Deploy\\Automation_Demo\\Test1_hello.exe";
+        runCmdCommand(cmdrun);
+        wait(2000);
+        boolean flag=false;
+        try {
+          flag =  compareFiles(expected,results);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Assert.assertTrue(flag);
+
+    }
+
+    public void runCmdCommand(String command){
+        try {
+            Runtime.getRuntime().exec(command);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                wait(1000);
+                Runtime.getRuntime().exec("taskkill /f /im cmd.exe") ;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void wait(int miliSeconds){
+        try {
+            Thread.sleep(miliSeconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean compareFiles(String file1, String file2) throws IOException{
+        BufferedReader reader1 = new BufferedReader(new FileReader(file1));
+
+        BufferedReader reader2 = new BufferedReader(new FileReader(file2));
+
+        String line1 = reader1.readLine();
+
+        String line2 = reader2.readLine();
+
+        boolean areEqual = true;
+
+        int lineNum = 1;
+
+        while (line1 != null || line2 != null)
+        {
+            if(line1 == null || line2 == null)
+            {
+                areEqual = false;
+
+                break;
+            }
+            else if(! line1.equalsIgnoreCase(line2))
+            {
+                areEqual = false;
+
+                break;
+            }
+
+            line1 = reader1.readLine();
+
+            line2 = reader2.readLine();
+
+            lineNum++;
+        }
+
+        if(areEqual)
+        {
+            System.out.println("Two files have same content.");
+            return true;
+        }
+        else
+        {
+            System.out.println("Two files have different content. They differ at line "+lineNum);
+
+            System.out.println("File1 has "+line1+" and File2 has "+line2+" at line "+lineNum);
+            return false;
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
